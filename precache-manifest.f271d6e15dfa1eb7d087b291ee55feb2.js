b61313fb9f513d71c4ef8d5e7ae7a291f4567713
Asfand Yar <asfi.official@gmail.com>
2019-12-14 22:14:32 +0500

    Updates

---
+++ precache-manifest.f271d6e15dfa1eb7d087b291ee55feb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162afb42bbfef4100f81b288cb1a4f58",
+    "url": "/sports-store/index.html"
+  },
+  {
+    "revision": "b512d9be9198cf124b3b",
+    "url": "/sports-store/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a42e769daada997f59d5",
+    "url": "/sports-store/static/css/main.7ec7f220.chunk.css"
+  },
+  {
+    "revision": "b512d9be9198cf124b3b",
+    "url": "/sports-store/static/js/2.b6e19962.chunk.js"
+  },
+  {
+    "revision": "a42e769daada997f59d5",
+    "url": "/sports-store/static/js/main.c5671941.chunk.js"
+  },
+  {
+    "revision": "1278ed5d296cf86f024e",
+    "url": "/sports-store/static/js/runtime-main.6c7c357b.js"
+  }
+]);
