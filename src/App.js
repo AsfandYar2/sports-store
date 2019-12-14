842d56cc970e858ca1f5539240832182ca600d65
Asfand Yar <asfi.official@gmail.com>
2019-12-14 22:09:00 +0500

    sports-store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 // import "materialize-css/dist/css/materialize.min.css";
@@ -13,7 +13,9 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        <HashRouter basename="/">
+          <Navbar />
+        </HashRouter>
         <Home />
       </Router>
     </div>
